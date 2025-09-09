9e585f1f30b259340ce6515fe74c0d6537ceb339
Juan Tizapa <juan.tizapa@carsol.mx>
2025-09-09 12:15:11 -0600

    Se realiza cosas para websocket en nest js

---
+++ nestjs/05-ws-cllient/src/main.ts
@@ -0,0 +1,14 @@
+import { connectToServer } from './socket-client'
+import './style.css' 
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h1> Websocket - Client</h1>
+
+    <span id="server-status">offline</span>
+    <ul id='clients-ul'>
+      <li>asdfads asdfaf</li>
+    </ul>
+  </div>
+`
+ connectToServer();
---
+++ nestjs/05-ws-cllient/src/socket-client.ts
@@ -0,0 +1,30 @@
+import {Manager, Socket} from 'socket.io-client'
+
+export const connectToServer = () =>{
+    const manager = new Manager('localhost:3000/socket.io/socket.io.js')
+    const socket = manager.socket('/');
+    addListeners(socket);
+}
+
+const addListeners = (socket: Socket) =>{
+    const serverStatusLabel = document.querySelector('#server-status');
+    const clientsUl = document.querySelector('#clients-ul')
+    socket.on('connect', () =>{
+        serverStatusLabel.innerHTML = 'connected'   
+     });
+
+     socket.on('disconnect', () =>{
+        serverStatusLabel.innerHTML = 'disconnected'
+    });
+
+    socket.on('clients-updated', (clients: string[]) =>{
+        let clientsHtml = '';
+        clients.forEach(clientId => {
+            clientsHtml += `
+                <li> ${clientId}</li>
+            `
+        });
+
+        clientsUl.innerHTML = clientsHtml
+    })
+}
